9313755911d89700bcbd8c0998b02ab2e7b0ff12
amanhussain <amanhus977@gmail.com>
2024-02-22 03:32:05 +0500

    assignment here

---
+++ app.js
@@ -0,0 +1,59 @@
+// chapter n01
+// question no1
+
+
+// question 2
+alert("Error! Please enter a valid password.")
+
+// Question no3
+alert("Welcome to JS Land...\n Happy Coding!")
+
+// Question no 4
+alert("Welcome to JS Land...")
+alert("Happy Coding!\n Prevent this page from creating additional dialogs.")
+
+//Question no 5
+alert("Hello...I can run JS throught my web browser's console")
+
+// capter n0 2
+// Qno4
+alert("Jhone Doe")
+alert("15 years old")
+alert("Certified Mobile application Development")
+
+//Qno5
+alert("PIZZA")
+alert("PIZZ")
+alert("PIZ")
+alert("PI")
+alert("P")
+
+//Qno6
+alert("My email address is example@amanhus123.com")
+
+//Qno7
+alert("I am tying to learn from the Book A smarter way to learn JAVAScript")
+
+//Qno8
+document.write("Yah!I can write HTML contect throught JavaScript")
+
+//Qno9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// chapter no3
+//Qno1
+alert("I am 15 year old")
+
+//Qno2
+var n = 14
+alert("you have visited this site " + n + " time")
+
+//Qno3
+var birthyear = 1990
+document.write("My birth year is " + birthyear);
+document.write("<br>")
+document.write("Data type of my ddedclared variable is number")
+
+//Qno4
+var Yah ="(s)"
+document.write("<br> <b> John Doe </b> "," ","ordered"," ","<b> 5 T-Shirt </b>",Yah, " " , "on XYZ Clothing store")
